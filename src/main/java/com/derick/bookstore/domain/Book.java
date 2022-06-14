9be93fba675d0c58e4fe968504c63e498cbc4bb8
derickcjesus <derickcjesus@gmail.com>
2022-06-14 10:06:10 -0300

    finalizing domain classes

--- src/main/java/com/derick/bookstore/domain/Book.java
+++ src/main/java/com/derick/bookstore/domain/Book.java
@@ -1,13 +1,27 @@
 package com.derick.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class Book implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String name_author;
 	private String text;
 
+	@JoinColumn(name = "category_id")
 	private Category category;
 
 	public Book() {
