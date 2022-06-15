9e0c062aefb0bbeae142afe1fdb5eaf69b7e7166
derickcjesus <derickcjesus@gmail.com>
2022-06-15 10:25:58 -0300

    deployed repositories

--- src/main/java/com/derick/bookstore/repository/BookRepository.java
+++ src/main/java/com/derick/bookstore/repository/BookRepository.java
@@ -3,9 +3,9 @@ package com.derick.bookstore.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.derick.bookstore.domain.Category;
+import com.derick.bookstore.domain.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Category, Integer>{
+public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
